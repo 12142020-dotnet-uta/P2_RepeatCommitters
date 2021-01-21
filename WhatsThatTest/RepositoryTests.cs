7b200ef6fe9c8947fb860368a243d79c25287920
BuildTools <unconfigured@null.spigotmc.org>
2021-01-21 14:46:06 -0800

    Starting Repository Testing

--- WhatsThatTest/RepositoryTests.cs
+++ WhatsThatTest/RepositoryTests.cs
@@ -1,12 +1,109 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ModelLayer.Models;
+using RepositoryLayer;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace WhatsThatTest
 {
-    class RepositoryTests
+    public class RepositoryTests
     {
+        private readonly ILogger<Repository> _logger;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void GetAllUsersAsyncTest()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "InHarmonyTestDB")
+            .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                Repository repository = new Repository(context, _logger);
+                var user = new User
+                {
+                    Id = int.MaxValue,
+                    UserName = "jtest",
+                    Password = "Test1!",
+                    FirstName = "Johnny",
+                    LastName = "Test",
+                    Email = "johnnytest123@email.com"
+                };
+
+                repository.users.Add(user);
+                var listOfUsers = repository.GetAllUsersAsync();
+                Assert.NotNull(listOfUsers);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void GetUserByIdAsyncTest()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void DoesUserExistTest()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void GetUserByNameAndPassTest()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void SaveUserToDbTest()
+        {
+        
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void CreateNewUserTest() 
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void GetAllMessagesAsyncTest()
+        {
+        
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void GetUsersByPartialNTest()
+        {
+        
+        }
     }
 }
