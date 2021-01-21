ec7e9bad6ba266767e4f66e6d3b51612ef162580
BuildTools <unconfigured@null.spigotmc.org>
2021-01-21 10:28:01 -0800

    Fix namespace

--- WhatsThatTest/ModelTests.cs
+++ WhatsThatTest/ModelTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Xunit;
 
-namespace Models.Tests
+namespace WhatsThatTest
 {
     public class ModelTests
     {
