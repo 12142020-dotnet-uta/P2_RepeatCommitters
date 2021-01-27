f6357cbae1d1543f99991a5d6a7748d5eb66c625
daytonschuh <daytonschuh@gmail.com>
2021-01-26 20:08:59 -0800

    Adding Tests for Controllers

---
+++ WhatsThatTest/ControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WhatsThatTest
+{
+    public class ControllerTests
+    {
+        [Fact]
+        public void Test() { }
+    }
+}
